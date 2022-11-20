472667d602861f78dbeb8bbb939fc8dc7e0747cf
raheemadamboev <vividricemaryandmax@gmail.com>
2022-11-20 18:07:22 +0500

    animated as state

--- app/src/main/java/xyz/teamgravity/composeanimations/Main.kt
+++ app/src/main/java/xyz/teamgravity/composeanimations/Main.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import xyz.teamgravity.composeanimations.ui.theme.ComposeAnimationsTheme
 
 class Main : ComponentActivity() {
@@ -31,7 +34,8 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AnimatedVisibilityDemo()
+//                    AnimatedVisibilityDemo()
+                    AnimateAsStateDemo()
                 }
             }
         }
@@ -40,6 +44,7 @@ class Main : ComponentActivity() {
     @Composable
     fun AnimatedVisibilityDemo() {
         Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxSize()
         ) {
             var visible by remember { mutableStateOf(false) }
@@ -60,6 +65,37 @@ class Main : ComponentActivity() {
             ) {
                 Box(modifier = Modifier.background(Color.Red))
             }
+        }
+    }
+
+    @Composable
+    fun AnimateAsStateDemo() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            var round by remember { mutableStateOf(false) }
+            val borderRadius by animateIntAsState(
+                targetValue = if (round) 100 else 0,
+                animationSpec = tween(
+                    durationMillis = 3_000,
+                    delayMillis = 500
+                )
+            )
+
+            Button(
+                onClick = {
+                    round = !round
+                }
+            ) {
+                Text(text = stringResource(id = R.string.toggle))
+            }
+            Box(
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(RoundedCornerShape(percent = borderRadius))
+                    .background(Color.Red)
+            )
         }
     }
 }
