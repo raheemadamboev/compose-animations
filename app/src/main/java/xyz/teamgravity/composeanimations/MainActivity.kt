2db192dd2b2d2fc7b9abca84c547e7b835088236
raheemadamboev <vividricemaryandmax@gmail.com>
2022-11-20 17:35:52 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/composeanimations/MainActivity.kt
@@ -0,0 +1,26 @@
+package xyz.teamgravity.composeanimations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import xyz.teamgravity.composeanimations.ui.theme.ComposeAnimationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAnimationsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
